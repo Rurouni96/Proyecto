53cf62a396805bffcc147d9e480d71f1892dda1c
Ruro2 <102768784+Rurouni96@users.noreply.github.com>
2022-04-07 14:49:52 -0600

    Agregar archivos de proyecto.

---
+++ Project/Migrations/202204072039184_AdicionTablePedido.cs
@@ -0,0 +1,35 @@
+﻿namespace Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdicionTablePedido : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Pedido",
+                c => new
+                    {
+                        PedidoId = c.Int(nullable: false, identity: true),
+                        FechaCreacion = c.DateTime(nullable: false),
+                        CLienteId = c.Int(nullable: false),
+                        DescuentoTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        SubTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Estado = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.PedidoId)
+                .ForeignKey("dbo.Cliente", t => t.CLienteId)
+                .Index(t => t.CLienteId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Pedido", "CLienteId", "dbo.Cliente");
+            DropIndex("dbo.Pedido", new[] { "CLienteId" });
+            DropTable("dbo.Pedido");
+        }
+    }
+}
---
+++ Project/Models/ClasificacionCliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class ClasificacionCliente
+    {
+        [Key]
+        [Required]
+        public int ClasificacionId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Codigo { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Descripcion { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+
+    }
+}
